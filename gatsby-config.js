cd32e7f3518458c8df5c5a3eeb43d4ed70b1144a
sebastian <sebasenlu@gmail.com>
2020-02-23 14:37:50 -0500

    config files completed

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,35 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        baseUrl: "olga.local",
+        protocol: "http",
+
+        restApiRoutePrefix: "wp-json",
+
+        hostingWPCOM: false,
+
+        useACF: true,
+
+        acfOptionPageIds: [],
+
+        perPage: 100,
+
+        concurrentRequests: 10,
+
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+          "**/users",
+        ],
+      },
+    },
+  ],
 }
---
+++ src/hooks/use-page.js
@@ -0,0 +1,11 @@
+// import { graphql,useStaticQuery } from "gatsby";
+
+// const usePage = () => {
+//     const data = useStaticQuery(graphql`
+
+//     `)
+
+//     return data
+// }
+
+// export default usePage;
